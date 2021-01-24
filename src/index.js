0396957d2016818d503a5f0067aad4530531cc68
Andrew Tsishevskii <at100991@student.sgh.waw.pl>
2021-01-24 18:52:58 +0200

    Do edit modal window

--- src/index.js
+++ src/index.js
@@ -13,3 +13,26 @@ project.addEventListener('mouseleave', () => {
   projectBtns.style = 'display: none';
   project.style = 'height: 98px';
 });
+
+//Show edit modal if user click on edit modal btn
+const editProjectBtn = document.querySelectorAll('.edit-btn');
+const editProjectModal = document.querySelector('.edit-modal-window');
+const editModalCloseBtn = editProjectModal.querySelector('.close');
+editProjectBtn.forEach((btn) =>
+  btn.addEventListener(
+    'click',
+    () => (editProjectModal.style = 'display:block')
+  )
+);
+
+editModalCloseBtn.addEventListener(
+  'click',
+  () => (editProjectModal.style = 'display: none')
+);
+
+window.addEventListener('click',(e) => {
+  if(e.target.className === 'main-page_edit-project-modal-window edit-modal-window') {
+    editProjectModal.style = 'display: none';
+  }
+})
+
