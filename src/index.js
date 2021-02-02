4ea7ce5ae44cdad8514ebc2b10e44152365a438b
Andrew Tsishevskii <at100991@student.sgh.waw.pl>
2021-02-02 14:50:17 +0200

    Do search bar functionality

--- src/index.js
+++ src/index.js
@@ -15,3 +15,38 @@ function renderProjectsElements() {
   );
 }
 
+activateSearchBar();
+
+function activateSearchBar() {
+  const searchInput = document.querySelector('.search-input');
+
+  searchInput.addEventListener('keyup', (e) => {
+    const mainPage = document.querySelector('.main-page');
+    const mainPageDisplayProperty = mainPage.style.display;
+    const searchQuery = e.target.value;
+    /*If mainPage display style dont equals 'none',we are on main page and search only project 
+    otherwise,we are on project page and search only tasks*/
+    if (mainPageDisplayProperty !== 'none') {
+      const projects = document.querySelectorAll('.main-page_project');
+      projects.forEach((project) => {
+        const projectTitle = project.querySelector('.project_title')
+          .textContent;
+        if (projectTitle.includes(searchQuery)) {
+          project.style = 'display: flex';
+        } else {
+          project.style = 'display: none';
+        }
+      });
+    } else {
+      const tasks = document.querySelectorAll('.task');
+      tasks.forEach((task) => {
+        const taskTitle = task.querySelector('.task_title').textContent;
+        if (taskTitle.includes(searchQuery)) {
+          task.style = 'display: flex';
+        } else {
+          task.style = 'display: none';
+        }
+      });
+    }
+  });
+}
